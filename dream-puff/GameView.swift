b3c9f6d40271567ac283c6dc178f22abd712406d
missKenzington <makenzie7elliott@gmail.com>
2018-04-28 03:07:31 -0600

    added open GL shaders and program

--- dream-puff/GameView.swift
+++ dream-puff/GameView.swift
@@ -25,12 +25,13 @@ class GameView: GLKViewController {
     private func setup() {
         glClearColor(0.0, 1.0, 0.0, 1.0)
         
-        let vertextShaderSource: NSString = "" +
-        "void main() \n" +
-        "{ \n" +
-        "   gl_Position = vec4(1.0, 1.0, 1.0, 1.0); \n" +
-        "} \n" +
-        "\n"
+        let vertextShaderSource: NSString = NSString(string: "void main() {\n" +
+        "gl_Position = vec4(1.0, 1.0, 0.0, 1.0);\n" +
+        "}\n")
+        
+        let fragmentShaderSource: NSString = NSString(string: "void main() {\n" +
+            "gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);\n" +
+            "}\n")
         
         
         // create and compile a vertex shader
@@ -38,12 +39,59 @@ class GameView: GLKViewController {
         var vertexShaderSourceUTF8 = vertextShaderSource.utf8String
         glShaderSource(vertexShader, 1, &vertexShaderSourceUTF8, nil)
         glCompileShader(vertexShader)
-        let vetexShaderCompileStatus: GLuint = GL_FALSE
+        var vetexShaderCompileStatus: GLint = GL_FALSE
+        glGetShaderiv(vertexShader, GLenum(GL_COMPILE_STATUS), &vetexShaderCompileStatus)
         
+        if (vetexShaderCompileStatus == GL_FALSE) {
+            // print error, shader cannot be cteaded
+            var vertexShaderLogLength: GLint = 0
+            glGetShaderiv(vertexShader, GLenum(GL_INFO_LOG_LENGTH), &vertexShaderLogLength)
+            // allocate memory for the string
+            let vertexShaderLog = UnsafeMutablePointer<GLchar>.allocate(capacity: Int(vertexShaderLogLength)) // deallocated happens automagically
+            glGetShaderInfoLog(vertexShader, vertexShaderLogLength, nil, vertexShaderLog)
+            let vertexShaderLogString: NSString? = NSString(utf8String: vertexShaderLog)!
+            print("Vertex shader compile failed! Error: \(String(describing: vertexShaderLogString))")
+            // TODO - prevent drwaing if we dont have a vertex shader
+        }
+    
         
         // create a compile a fragmetn shader
+        let fragmentShader: GLuint = glCreateShader(GLenum(GL_FRAGMENT_SHADER))
+        var fragmentShaderSourceUTF8 = fragmentShaderSource.utf8String
+        glShaderSource(fragmentShader, 1, &fragmentShaderSourceUTF8, nil)
+        glCompileShader(fragmentShader)
+        var fragmentShaderCompileStatus: GLint = GL_FALSE
+        glGetShaderiv(fragmentShader, GLenum(GL_COMPILE_STATUS), &fragmentShaderCompileStatus)
+        
+        if (fragmentShaderCompileStatus == GL_FALSE) {
+            // print error, shader cannot be cteaded
+            var fragmentShaderLogLength: GLint = 0
+            glGetShaderiv(fragmentShader, GLenum(GL_INFO_LOG_LENGTH), &fragmentShaderLogLength)
+            // allocate memory for the string
+            let fragmentShaderLog = UnsafeMutablePointer<GLchar>.allocate(capacity: Int(fragmentShaderLogLength)) // deallocated happens automagically
+            glGetShaderInfoLog(fragmentShader, fragmentShaderLogLength, nil, fragmentShaderLog)
+            let fragmentShaderLogString: NSString? = NSString(utf8String: fragmentShaderLog)!
+            print("Fragment shader compile failed! Error: \(String(describing: fragmentShaderLogString))")
+            // TODO - prevent drwaing if we dont have a fragment shader
+        }
         
         // link shaders into program
+        let program: GLuint = glCreateProgram()
+        glAttachShader(program, vertexShader)
+        glAttachShader(program, fragmentShader)
+        glLinkProgram(program)
+        var programLinkStatus: GLint = GL_FALSE
+        glGetProgramiv(program, GLenum(GL_LINK_STATUS), &programLinkStatus)
+        if (programLinkStatus == GL_FALSE){
+            var programLogLength: GLint = 0
+            glGetProgramiv(program, GLenum(GL_INFO_LOG_LENGTH), &programLogLength)
+            let programLog = UnsafeMutablePointer<GLchar>.allocate(capacity: Int(programLogLength))
+            glGetProgramInfoLog(program, programLogLength, nil, programLog)
+            let programLogString: NSString = NSString(utf8String: programLog)!
+            print("Program link failed! Error: \(programLogString)")
+        }
+        
+        glUseProgram(program)
         
     }
     
@@ -52,7 +100,7 @@ class GameView: GLKViewController {
         
     }
     
-    // display
+    // update display
     override func glkView(_ view: GLKView, drawIn rect: CGRect) {
         glClear(GLbitfield(bitPattern: GL_COLOR_BUFFER_BIT))
         
